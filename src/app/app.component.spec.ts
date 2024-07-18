d0e52063013ab807b7e00fac3f5eb65388923547
tanveer alam <mohammadtanveer0289@gmail.com>
2024-07-19 01:24:08 +0530

    initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'YumYard' title`, () => {
+  it(`should have the 'swiggy' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('YumYard');
+    expect(app.title).toEqual('swiggy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, YumYard');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, swiggy');
   });
 });
